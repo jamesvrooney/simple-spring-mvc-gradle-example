f7d4cace990a96864eeafc82ebb3d59318113039
jamesvrooney <jamesvrooney@hotmail.com>
2017-01-25 11:21:51 +0000

    Create new test database

---
+++ model/src/main/resources/sql/create_employee_table.sql
@@ -0,0 +1,10 @@
+--CREATE DATABASE school_test_db;
+
+CREATE TABLE EMPLOYEE (
+    id INT NOT NULL auto_increment,
+    name VARCHAR(50) NOT NULL,
+    joining_date DATE NOT NULL,
+    salary DOUBLE NOT NULL,
+    ssn VARCHAR(30) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
