874078e96696616583bade524472a32214283ff1
jamesvrooney <jamesvrooney@hotmail.com>
2017-02-21 16:16:56 +0000

    Added test for EmployeeService (not implemented)

---
+++ service/src/test/java/com/rooney/james/service/EmployeeServiceTest.java
@@ -0,0 +1,27 @@
+package com.rooney.james.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeServiceTest {
+    @Test
+    public void saveEmployee() throws Exception {
+        // TODO - implement this test
+    }
+
+    @Test
+    public void findAllEmployees() throws Exception {
+        // TODO - implement this test
+    }
+
+    @Test
+    public void deleteEmployeeBySsn() throws Exception {
+        // TODO - implement this test
+    }
+
+    @Test
+    public void findBySsn() throws Exception {
+        // TODO - implement this test
+    }
+}
